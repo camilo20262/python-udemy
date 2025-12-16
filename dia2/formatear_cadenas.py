3715959f252169043b7388455f09c9b5dc5afe32
Camilo Diaz <camilo20263@gmail.com>
2025-12-16 11:29:19 -0500

    format1.2

--- dia2/formatear_cadenas.py
+++ dia2/formatear_cadenas.py
@@ -2,3 +2,13 @@ x =5
 y = 10
 
 print("mi primer valor es {} y mi segundo valor es {}".format(x,y))
+
+
+
+color="rojo"
+placa="UIQ32F"
+
+print(f"Mi moto es de color  {color} y su placa es {placa}")
+
+
+
