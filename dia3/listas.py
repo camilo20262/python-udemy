9a06108bb4caf1d4ba55daab392050c6dc38fb89
Camilo Diaz <camilo20263@gmail.com>
2025-12-17 11:11:24 -0500

    unir listas

--- dia3/listas.py
+++ dia3/listas.py
@@ -1,5 +1,10 @@
 mi_lista=["a","b","c","d","e"]
+print(mi_lista)
 resultado= len(mi_lista) # Devuelve la cantidad de elementos en la lista
 print(resultado)
 print(mi_lista[0]) # Accede al primer elemento de la lista
 print(mi_lista[-1]) # Accede al ultimo elemento de la lista
+print(mi_lista[1:4]) # Accede a una porcion de la lista desde el indice 1 hasta el 3
+mi_lista2=["f","g","h","i","j"]
+print(mi_lista+mi_lista2) # Une dos listas
+mi_lista.extend(mi_lista2) # Agrega los elementos de una lista al final de otra
