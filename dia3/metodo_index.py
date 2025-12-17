a631af7151aad7271ec2a070267af62312f09474
Camilo Diaz <camilo20263@gmail.com>
2025-12-17 10:17:16 -0500

    rindex

--- dia3/metodo_index.py
+++ dia3/metodo_index.py
@@ -4,7 +4,14 @@ print(resultado)# Toma el caracter en la posicion 2
 resultado =mi_texto[-1]# Toma el ultimo caracter 
 print(resultado)# Toma el caracter en la posicion -1
 
-
-texto2="Python"#cadena de texto
-resultado2= texto2.index("P")# Devuelve la posicion del caracter "P"
+texto="Python"
+texto2="hola me llamo camila"#cadena de texto
+resultado2= texto.index("P")# Devuelve la posicion del caracter "P"
 print(resultado2)# Imprime 0
+resultado2= texto2.rindex("a")# Devuelve la posicion del ultimo caracter "a"
+print(resultado2)# Imprime 16
+
+
+
+
+
