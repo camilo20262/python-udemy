76203bbf6003be4812e59a554e4fc7648a42c3a8
Camilo Diaz <camilo20263@gmail.com>
2025-12-17 10:39:43 -0500

    capitalize

--- dia3/metodos_string.py
+++ dia3/metodos_string.py
@@ -1,5 +1,8 @@
 texto= "esto es un texto de preuba para probar funciones de strings"
-prueba = texto.upper()
+prueba = texto.upper() # Convierte todo a mayusculas
 print(prueba)
-prueba = texto.lower()
+prueba = texto.lower() # Convierte todo a minusculas
+print(prueba)
+
+prueba = texto.lower().capitalize() # Convierte la primera letra en mayuscula
 print(prueba)
