23da207c7da53ac9a5f6faaf310d7b63d53aa2f1
Camilo Diaz <camilo20263@gmail.com>
2025-12-17 12:16:17 -0500

    apellido

--- dia3/diccionarios.py
+++ dia3/diccionarios.py
@@ -2,3 +2,9 @@ dic1={ 'c1':'valor1', 'c2':'valor2', 'c3':'valor3'}
 print(dic1)
 valor= dic1['c2'] # Accede al valor de la clave 'c2'
 print(valor)
+
+
+
+cliente = {'nombre': 'Juan', 'apellido': 'Fuentes','peso':88, 'talla':1.75}
+consulta = (cliente ['apellido']) # Accede al valor de la clave 'apellido'
+print(consulta)
