ac5cdb59f5e3cfe8b9974c4d58391255b6e95e66
Camilo Diaz <camilo20263@gmail.com>
2025-12-17 11:08:53 -0500

    listas

---
+++ dia3/listas.py
@@ -0,0 +1,5 @@
+mi_lista=["a","b","c","d","e"]
+resultado= len(mi_lista) # Devuelve la cantidad de elementos en la lista
+print(resultado)
+print(mi_lista[0]) # Accede al primer elemento de la lista
+print(mi_lista[-1]) # Accede al ultimo elemento de la lista
