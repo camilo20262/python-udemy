7392ca2834821808dad4760650b14f6ce599d39f
Camilo Diaz <camilo20263@gmail.com>
2025-12-17 10:12:16 -0500

    indices

--- dia3/metodo_index.py
+++ dia3/metodo_index.py
@@ -1,3 +1,10 @@
 mi_texto="Esta es una preuba"#cadena de texto
 resultado =mi_texto[2] 
 print(resultado)# Toma el caracter en la posicion 2
+resultado =mi_texto[-1]# Toma el ultimo caracter 
+print(resultado)# Toma el caracter en la posicion -1
+
+
+texto2="Python"#cadena de texto
+resultado2= texto2.index("P")# Devuelve la posicion del caracter "P"
+print(resultado2)# Imprime 0
