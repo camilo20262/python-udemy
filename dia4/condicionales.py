8a2831fd24cd6944a2f15285d77d7888d17f51a4
Camilo Diaz <camilo20263@gmail.com>
2025-12-18 10:45:28 -0500

    condicionales

---
+++ dia4/condicionales.py
@@ -0,0 +1,9 @@
+mascota= input("Que mascota tienes? ")
+if mascota.lower() == "perro":
+    print("Tienes un perro")
+elif mascota.lower() =="gato":
+    print("Tienes un gato")
+elif mascota.lower()== "loro":
+    print("Tienes un loro")
+else:
+    print("tienes un otro tipo de mascota")
