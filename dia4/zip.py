97561b17f400a134204adf40455b8fbcefb13e10
Camilo Diaz <camilo20263@gmail.com>
2025-12-18 13:04:07 -0500

    combinaciones de listas

--- dia4/zip.py
+++ dia4/zip.py
@@ -1,4 +1,8 @@
 nombres=['Camilo', 'Ana', 'Juan', 'Pedro'] #lista de nombres
 edades=[25,30,22,28] #lista de edades
-combinados= list(zip(nombres,edades)) #combinar las dos listas en una sola usando zip 
+ciudades=['Bogota', 'Madrid', 'Mexico', 'Lima'] #lista de ciudades
+combinados= list(zip(nombres,edades,ciudades)) #combinar las tres listas en una sola usando zip 
 print(combinados) #imprimir el objeto zip 
+print("-------------------")
+for nombres,edades,ciudades in combinados: #iterar sobre cada tupla en el objeto zip
+    print(f"{nombres} tiene {edades} años y vive en {ciudades}") #imprimir el nombre, edad y ciudad de cada persona
