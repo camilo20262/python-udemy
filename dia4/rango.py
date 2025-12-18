662cc0162e68e7328a16cc6feb1bff2575b09d43
Camilo Diaz <camilo20263@gmail.com>
2025-12-18 12:18:25 -0500

    rango

---
+++ dia4/rango.py
@@ -0,0 +1,9 @@
+for i in range (11): # 0 a 10
+    print(i)
+
+print("--------------")
+for i in range (1,11):# 1 a 10
+    print(i)
+print("--------------")
+for i in range (1,21,2):# 1 a 20 de 2 en 2
+    print(i)
