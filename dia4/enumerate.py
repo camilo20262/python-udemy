5adb8c1cbd40078293c809f405a6665f626d2ea9
Camilo Diaz <camilo20263@gmail.com>
2025-12-18 12:33:06 -0500

    crear tupla

--- dia4/enumerate.py
+++ dia4/enumerate.py
@@ -1,3 +1,10 @@
 lista= ['a', 'b', 'c', 'd', 'e'] #lista de letras
 for indice, letra in enumerate(lista): #iterar sobre cada letra en la lista con su indice
     print(indice,letra) #imprimir el indice y la letra
+
+
+
+print("-------------------")
+tupla=list(enumerate(lista))#crear una tupla de indices y letras usando enumerate
+print(tupla) #imprimir la tupla de indices y letras
+print(tupla[2][0]) #imprimir el indice del tercer elemento
