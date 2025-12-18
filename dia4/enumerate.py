f8e4594f2d22051d8ff0b34eb7810875a82d207b
Camilo Diaz <camilo20263@gmail.com>
2025-12-18 12:29:51 -0500

    enumerar

---
+++ dia4/enumerate.py
@@ -0,0 +1,3 @@
+lista= ['a', 'b', 'c', 'd', 'e'] #lista de letras
+for indice, letra in enumerate(lista): #iterar sobre cada letra en la lista con su indice
+    print(indice,letra) #imprimir el indice y la letra
