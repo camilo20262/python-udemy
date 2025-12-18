4fcefa4f4bb2e41827b3a9b58b0d1df580f7c556
Camilo Diaz <camilo20263@gmail.com>
2025-12-18 12:17:10 -0500

    comentar codigo

--- dia4/while.py
+++ dia4/while.py
@@ -1,5 +1,16 @@
-monedas =5
+monedas =5 #cantidad de monedas iniciales
 
-while monedas >0:
-    print(f"tengo {monedas} monedas")
-    monedas -=1
+while monedas >0:#mientras tenga monedas
+    print(f"tengo {monedas} monedas") #imprimir la cantidad de monedas
+    monedas -=1#restar una moneda
+else:
+    print("Ya no tengo monedas")#imprimir cuando ya no hay monedas
+
+
+respuesta="s"#respuesta inicial para el bucle
+
+while respuesta =="s":#mientras la respuesta sea "s"
+    respuesta = input("Quieres continuar? (s/n): ")#preguntar si quiere continuar
+    if respuesta !="s":#si la respuesta no es "s"
+        print("Gracias por participar") #imprimir mensaje de agradecimiento
+
