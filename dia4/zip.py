ce7e431897bab6d96f3b576a219a5f65033a00d7
Camilo Diaz <camilo20263@gmail.com>
2025-12-18 13:00:25 -0500

    zip

---
+++ dia4/zip.py
@@ -0,0 +1,4 @@
+nombres=['Camilo', 'Ana', 'Juan', 'Pedro'] #lista de nombres
+edades=[25,30,22,28] #lista de edades
+combinados= list(zip(nombres,edades)) #combinar las dos listas en una sola usando zip 
+print(combinados) #imprimir el objeto zip 
