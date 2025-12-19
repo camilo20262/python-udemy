c8a0c7f02882a0cbdd4d0fd3172be87012ea4540
Camilo Diaz <camilo20263@gmail.com>
2025-12-19 10:15:25 -0500

    primeras funciones

---
+++ dia5/crear_funciones.py
@@ -0,0 +1,8 @@
+def saludar_persona(nombre):
+    print("Hola, como estas? "+nombre)
+
+saludar_persona("Camila")
+
+def bienvenida(nombre_persona):
+    print(f"¡Bienvenido {nombre_persona}!")
+bienvenida("Andres")
