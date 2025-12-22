4a6eb3e406d80071c92874aa5ec27228c5b01830
Camilo Diaz <camilo20263@gmail.com>
2025-12-22 12:13:08 -0500

    borrar carpeta desde python

--- dia6/directorios.py
+++ dia6/directorios.py
@@ -7,7 +7,10 @@ archivo = open('clarol.txt','r')# abrir el archivo en modo lectura
 print(archivo.read()) #leer y mostrar el contenido del archivo
 archivo.close() #cerrar el archivo
 
-ruta2 = os.makedirs('C:\\Users\\camif\\herramientas\\nueva_carpeta') #crear un nuevo directorio en la ruta especificada
+#ruta2 = os.makedirs('C:\\Users\\camif\\herramientas\\nueva_carpeta') #crear un nuevo directorio en la ruta especificada
+
+
+os.rmdir('C:\\Users\\camif\\herramientas\\nueva_carpeta') #eliminar el directorio especificado
 
 
 
