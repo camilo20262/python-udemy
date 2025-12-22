595e8fb350ae1b1547d7e5634962cc607afb5220
Camilo Diaz <camilo20263@gmail.com>
2025-12-22 11:24:33 -0500

    crear nuevo archivo con el write

--- dia6/escribir_archivos.py
+++ dia6/escribir_archivos.py
@@ -1,3 +1,10 @@
-mi_archivo = open('prueba.txt','w')
-mi_archivo.write("Hola como estas?")
-mi_archivo.close()
+mi_archivo = open('prueba.txt','w') #abrir el archivo en modo escritura
+mi_archivo.write("Hola como estas?")# sobre escribir en el archivo
+mi_archivo.close() #cerrar el archivo
+
+
+mi_archivo2 = open('prueba1.txt','w')
+mi_archivo2.write("""Primera linea
+segunda linea
+                  tercera linea""") #escribir primera linea
+mi_archivo.close() #cerrar el archivo
