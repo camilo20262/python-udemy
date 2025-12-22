bf394abe66b0f180884fc176ab2db59befdd2f27
Camilo Diaz <camilo20263@gmail.com>
2025-12-22 10:56:04 -0500

    traer primera linea de un archivo

--- dia6/entrada_salida.py
+++ dia6/entrada_salida.py
@@ -1,5 +1,14 @@
-mi_archivo= open('prueba.txt') #abrir el archivo en modo lectura por defecto
-contenido= mi_archivo.read() #leer todo el contenido del archivo
-print(contenido) #imprimir el contenido en la consola
-mi_archivo.close() #cerrar el archivo
+#mi_archivo= open('prueba.txt') #abrir el archivo en modo lectura por defecto
+#contenido= mi_archivo.read() #leer todo el contenido del archivo
+#print(contenido) #imprimir el contenido en la consola
+#mi_archivo.close() #cerrar el archivo
+
+mi_archivo = open("prueba.txt", encoding="utf-8")
+
+primera_linea = mi_archivo.readline()  # leer la primera línea
+print(primera_linea)
+
+mi_archivo.close()
+
+
 
