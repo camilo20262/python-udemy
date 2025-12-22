832b74915336ca30c5f6b6ef2e8c148392f93629
Camilo Diaz <camilo20263@gmail.com>
2025-12-22 10:51:41 -0500

    abrir y cerrar archivo

--- dia6/entrada_salida.py
+++ dia6/entrada_salida.py
@@ -1,3 +1,5 @@
-mi_archivo= open('prueba.txt')
-print(mi_archivo.read())
+mi_archivo= open('prueba.txt') #abrir el archivo en modo lectura por defecto
+contenido= mi_archivo.read() #leer todo el contenido del archivo
+print(contenido) #imprimir el contenido en la consola
+mi_archivo.close() #cerrar el archivo
 
