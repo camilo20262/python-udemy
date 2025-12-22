29d71d621e313ad01c21c7e0874bee46726362e8
Camilo Diaz <camilo20263@gmail.com>
2025-12-22 13:24:27 -0500

    funcion sobre escribir

--- dia6/funciones_con_archivos.py
+++ dia6/funciones_con_archivos.py
@@ -6,3 +6,14 @@ def abrir_leer(ruta):
 
 texto= abrir_leer('prueba.txt')
 print(texto)
+
+
+print("-----")
+
+def sobrescribir(ruta):
+    archivo= open(ruta,'w')
+    contenido= archivo.write("contenido eliminado")
+    archivo.close()
+    return contenido
+sobrescribir('prueba1.txt')
+print(abrir_leer('prueba1.txt'))
