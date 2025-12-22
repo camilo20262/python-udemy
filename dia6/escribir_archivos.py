4a7e3047221e83c132c9629d32d35ff30cfde0d3
Camilo Diaz <camilo20263@gmail.com>
2025-12-22 11:31:24 -0500

    escribir al final del archivo

--- dia6/escribir_archivos.py
+++ dia6/escribir_archivos.py
@@ -3,8 +3,12 @@ mi_archivo.write("Hola como estas?")# sobre escribir en el archivo
 mi_archivo.close() #cerrar el archivo
 
 
-mi_archivo2 = open('prueba1.txt','w')
-mi_archivo2.write("""Primera linea
+mi_archivo2 = open('prueba1.txt','w')# abrir el archivo en modo escritura
+mi_archivo2.write("""Primera linea 
 segunda linea
                   tercera linea""") #escribir primera linea
 mi_archivo.close() #cerrar el archivo
+
+mi_archivo3 = open('prueba.txt','a') #abrir el archivo en modo agregar
+mi_archivo3.write('\n yo estoy muy bien') #agregar una nueva linea al final del archivo
+mi_archivo3.close() #cerrar el archivo
