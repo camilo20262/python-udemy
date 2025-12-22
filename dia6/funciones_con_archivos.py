7824d46e6ea2d1798d789eae611fe22a6b5d87a7
Camilo Diaz <camilo20263@gmail.com>
2025-12-22 13:31:49 -0500

    agregar contenido al final

--- dia6/funciones_con_archivos.py
+++ dia6/funciones_con_archivos.py
@@ -1,19 +1,32 @@
-def abrir_leer(ruta):
-    archivo = open(ruta, 'r')
-    contenido = archivo.read()
-    archivo.close()
-    return contenido
+def abrir_leer(ruta): #función para abrir y leer un archivo
+    archivo = open(ruta, 'r') #abrir el archivo en modo lectura
+    contenido = archivo.read() #leer el contenido del archivo 
+    archivo.close()#cerrar el archivo 
+    return contenido#retornar el contenido leído
 
-texto= abrir_leer('prueba.txt')
-print(texto)
+texto= abrir_leer('prueba.txt') #llamar a la función para abrir y leer el archivo prueba.txt
+print(texto) #imprimir el contenido leído
 
 
 print("-----")
 
-def sobrescribir(ruta):
-    archivo= open(ruta,'w')
-    contenido= archivo.write("contenido eliminado")
+def sobrescribir(ruta):#función para sobrescribir el contenido de un archivo
+    archivo= open(ruta,'w') #abrir el archivo en modo escritura
+    contenido= archivo.write("contenido eliminado") #sobrescribir el contenido del archivo
+    archivo.close() #cerrar el archivo
+    return contenido #retornar el contenido sobrescrito
+sobrescribir('prueba1.txt') #llamar a la función para sobrescribir el archivo prueba1.txt
+print(abrir_leer('prueba1.txt')) #leer y mostrar el contenido del archivo prueba1.txt después de sobrescribirlo
+
+
+print("-----")
+
+def registro_error(ruta):
+    archivo = open(ruta,'a')
+    contenido = archivo.write("\nse ha registrado un error de ejecución")
     archivo.close()
     return contenido
-sobrescribir('prueba1.txt')
+
+registro_error('prueba1.txt')
 print(abrir_leer('prueba1.txt'))
+    
