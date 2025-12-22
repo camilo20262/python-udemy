0179735df2c6b6f913fc2889238e813fa4686d11
Camilo Diaz <camilo20263@gmail.com>
2025-12-22 11:04:46 -0500

    crear una lista a partir de un archivo

--- dia6/entrada_salida.py
+++ dia6/entrada_salida.py
@@ -2,7 +2,7 @@ mi_archivo2= open('prueba.txt') #abrir el archivo en modo lectura por defecto
 contenido= mi_archivo2.read() #leer todo el contenido del archivo
 print(contenido) #imprimir el contenido en la consola
 mi_archivo2.close() #cerrar el archivo
-
+print("-----")
 mi_archivo = open("prueba.txt", encoding="utf-8")
 
 primera_linea = mi_archivo.readline()  # leer la primera línea
@@ -11,6 +11,17 @@ segunda_linea = mi_archivo.readline() # leer la segunda línea
 print(segunda_linea)
 
 mi_archivo.close()
+
+
+print("-----")
+mi_archivo3 = open("prueba.txt", encoding="utf-8")
+lineas = mi_archivo3.readlines()  # leer todas las líneas y guardarlas en una lista
+print(lineas)# imprimir la lista de líneas
+mi_archivo3.close() # #cerrar el archivo
+
+
+
+
 
 
 
