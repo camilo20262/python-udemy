b0eae5ef323a25e57233925fbf3a9e4f50a0cec5
Camilo Diaz <camilo20263@gmail.com>
2025-12-23 14:15:34 -0500

    herencia

--- dia7/herencia.py
+++ dia7/herencia.py
@@ -1,9 +1,15 @@
 class Animal:
+    def __init__(self,color,edad):
+        self.color = color
+        self.edad = edad
+
     def nacer(self):
         print("Este animal ha nacido")
 
 class Pajaro(Animal):
     pass 
 
-piolin= Pajaro()
+piolin= Pajaro("amarillo",3)
 piolin.nacer()
+print("este animal es de color "+piolin.color)
+print("este animal tiene "+str(piolin.edad)+" años")
