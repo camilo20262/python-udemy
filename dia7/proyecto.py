69d9a60ebebfad74b79c3125b675597b557a4ede
Camilo Diaz <camilo20263@gmail.com>
2025-12-23 16:01:26 -0500

    terminar proyecto

--- dia7/proyecto.py
+++ dia7/proyecto.py
@@ -22,3 +22,34 @@ class Cliente(Persona):
         else:
             self.balance -= retirar_cantidad
             print(f"Retiro de {retirar_cantidad} realizado. Nuevo balance: {self.balance}")
+
+
+
+nombre=input("Ingrese su nombre: ")
+apellido=input("Ingrese su apellido: ") 
+numero_cuenta=input("Ingrese su número de cuenta: ")
+balance=float(input("Ingrese su balance inicial: "))
+cliente = Cliente(nombre,apellido,numero_cuenta,balance)
+while True:
+  
+    
+    print("\n--- Menú de Banco ---")
+    print("1. Mostrar información del cliente")
+    print("2. Depositar dinero")
+    print("3. Retirar dinero")
+    print("4. Salir")
+    
+    opcion = input("Seleccione una opción (1-4): ")
+    if opcion == "1":
+        cliente.mostrar_cliente()
+    elif opcion == "2":
+        depositar =float(input("Ingrese la cantidad a depositar: "))
+        cliente.depositar(depositar)
+    elif opcion == "3":
+        retirar = float(input("Ingrese la cantidad a retirar: "))
+        cliente.retirar(retirar)
+    elif opcion == "4":
+        print("Gracias por usar el sistema bancario. ¡Hasta luego!")
+        break
+    else:
+        print("Opción no válida. Por favor, intente de nuevo.")
