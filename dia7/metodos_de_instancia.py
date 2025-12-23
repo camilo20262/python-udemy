888b03ffb167b70e8a97b6103027a9bf0ca475e5
Camilo Diaz <camilo20263@gmail.com>
2025-12-23 13:40:44 -0500

    metodos de instancia

---
+++ dia7/metodos_de_instancia.py
@@ -0,0 +1,21 @@
+class Pajaro: # Definición de la clase Pajaro
+    alas = True # Atributo de clase
+
+    def __init__(self,color,especie): # Método constructor
+        self.color =color # Atributo color
+        self.especie = especie # Atributo especie
+    
+    def piar(self): # Método piar
+        print("Pio Pio, Mi color es "+self.color+" y mi especie es "+self.especie)
+
+    def volar(self,metros): # Método volar
+        print(f"El pájaro ha volado {metros} metros")
+
+    def pintar_negro(self):
+        self.color = "negro"
+        print("el pajaro es de color "+self.color)
+
+piolin = Pajaro("Amarillo","Canario") # Crear una instancia de la clase Pajaro
+
+
+piolin.pintar_negro()
