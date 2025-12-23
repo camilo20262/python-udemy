ab7879a832c06c8942f67de53bdf06f39daa8bcd
Camilo Diaz <camilo20263@gmail.com>
2025-12-23 13:01:44 -0500

    segunda clase de casa

--- dia7/atributos.py
+++ dia7/atributos.py
@@ -10,3 +10,14 @@ mi_pajaro = Pajaro("Verde","Loro") # Crear una instancia de la clase Pajaro
 print(mi_pajaro.color)  # VERDE
 print(mi_pajaro.especie)  # Loro
 print(f"Tengo un pajaro de color {mi_pajaro.color} y de especie {mi_pajaro.especie}") # Tengo un pajaro de color Verde y de especie Loro
+
+
+class Casa:
+    def __init__(self,color,cantidad_pisos):
+        self.color =color 
+        self.cantidad_pisos = cantidad_pisos
+        
+casa_blanca =Casa("blanco",4)
+
+print(casa_blanca.color)  # blanco
+print(casa_blanca.cantidad_pisos)  # 4
