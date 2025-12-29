bbb986637edc857f9935b9586aeed054037686cf
Camilo Diaz <camilo20263@gmail.com>
2025-12-29 12:46:11 -0500

    contar numeros repetidos

---
+++ dia9/collection.py
@@ -0,0 +1,5 @@
+from collections import Counter   # importar collections
+
+numeros = [1,2,5,4,5,6,8,7,8,5,6,2,3,1,4,2,5,8,9,7,4,23,6] # lista de numeros 
+
+print(Counter(numeros)) # contar cuantas veces se repite en la lista 
