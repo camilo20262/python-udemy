0ece2840829e077d8406de324abdef8fa55eab29
Camilo Diaz <camilo20263@gmail.com>
2025-12-29 12:48:04 -0500

    contar caracteres

--- dia9/collection.py
+++ dia9/collection.py
@@ -3,3 +3,6 @@ from collections import Counter   # importar collections
 numeros = [1,2,5,4,5,6,8,7,8,5,6,2,3,1,4,2,5,8,9,7,4,23,6] # lista de numeros 
 
 print(Counter(numeros)) # contar cuantas veces se repite en la lista 
+
+frase ="al pan pan y al vino vino"
+print(Counter(frase.split()))
