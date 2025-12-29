88aa71960e96c4908cba89853572729855b2f49b
Camilo Diaz <camilo20263@gmail.com>
2025-12-29 13:14:33 -0500

    mover archivo a una ruta especifica

--- dia9/modulo_os_shutil.py
+++ dia9/modulo_os_shutil.py
@@ -1,5 +1,8 @@
 import os
+import shutil
 
 archivo = open('curso.txt','w')
 archivo.write('Es un texto de prueba')
 archivo.close()
+
+shutil.move('curso.txt', "C:\\Users\\camif\\OneDrive\\Escritorio")
