88c682564c606b7ffc3de8fdd986d0855dfb1e33
Camilo Diaz <camilo20263@gmail.com>
2025-12-29 13:21:13 -0500

    borrar archivo

--- dia9/modulo_os_shutil.py
+++ dia9/modulo_os_shutil.py
@@ -1,8 +1,12 @@
 import os
 import shutil
+import send2trash
 
 archivo = open('curso.txt','w')
 archivo.write('Es un texto de prueba')
 archivo.close()
 
-shutil.move('curso.txt', "C:\\Users\\camif\\OneDrive\\Escritorio")
+#shutil.move('curso.txt', "C:\\Users\\camif\\OneDrive\\Escritorio")
+
+
+send2trash.send2trash('curso.txt')
