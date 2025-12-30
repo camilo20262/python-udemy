93e4ea2033317bd50a57f15a941b128b2b3f3c9d
Camilo Diaz <camilo20263@gmail.com>
2025-12-30 14:31:58 -0500

    crear varios enemigos

--- dia10/main.py
+++ dia10/main.py
@@ -1,140 +1,130 @@
 import pygame
-import random 
-import math 
-#inicializar pygame
+import random
+import math
+
+# inicializar pygame
 pygame.init()
 
-#crear la pantalla
+# crear la pantalla
+pantalla = pygame.display.set_mode((800, 600))
 
-pantalla= pygame.display.set_mode((800,600))
-
-
-#titulo e icono
-
-pygame.display.set_caption("Invasion espacial")
-icono= pygame.image.load("ovni.png")
+# título e icono
+pygame.display.set_caption("Invasión Espacial")
+icono = pygame.image.load("ovni.png")
 pygame.display.set_icon(icono)
-fondo= pygame.image.load("Fondo.jpg")
+fondo = pygame.image.load("Fondo.jpg")
 
-#jugador
+# jugador
 img_jugador = pygame.image.load("cohete.png")
-jugador_x= 368
+jugador_x = 368
 jugador_y = 528
-jugador_x_cambio=0
+jugador_x_cambio = 0
 
-#enemigo
-img_enemigo = pygame.image.load("enemigo.png")
-enemigo_x= random.randint(0,736)
-enemigo_y = random.randint(50,200)
-enemigo_x_cambio=0.3
-enemigo_y_cambio=50
+# enemigo
+img_enemigo = []
+enemigo_x = []
+enemigo_y = []
+enemigo_x_cambio = []
+enemigo_y_cambio = []
+cantidad_enemigos = 8
 
+for e in range(cantidad_enemigos):
+    img_enemigo.append(pygame.image.load("enemigo.png"))
+    enemigo_x.append(random.randint(0, 736))
+    enemigo_y.append(random.randint(50, 200))
+    enemigo_x_cambio.append(0.3)
+    enemigo_y_cambio.append(50)
 
-#bala
+# bala
 img_bala = pygame.image.load("bala.png")
-bala_x= 0
+bala_x = 0
 bala_y = 500
-bala_x=0
-bala_y_cambio=1
-bala_visible = False 
+bala_y_cambio = 1
+bala_visible = False
 
-#puntaje
-puntaje=0 
+# puntaje
+puntaje = 0
 
+# funciones
+def jugador(x, y):
+    pantalla.blit(img_jugador, (x, y))
 
-#funcion jugador
-def jugador(x,y):
-    pantalla.blit(img_jugador,(x,y))
+def enemigo(x, y, ene):
+    pantalla.blit(img_enemigo[ene], (x, y))
 
-
-#funcion enemigo
-def enemigo(x,y):
-    pantalla.blit(img_enemigo,(x,y))
-
-#funcion disparar bala
-def disparar_bala(x,y):
+def disparar_bala(x, y):
     global bala_visible
-    bala_visible= True 
-    pantalla.blit(img_bala,(x+16,y+10))
+    bala_visible = True
+    pantalla.blit(img_bala, (x + 16, y + 10))
 
+def hay_colision(x1, y1, x2, y2):
+    distancia = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+    return distancia < 27
 
-# funcion dectetar coliciones 
-
-def hay_colision(x_1,y_1,x_2,y_2):
-    distancia=  math.sqrt(math.pow(x_1-x_2, 2) + math.pow ( y_2 - y_1, 2))
-    if distancia < 27:
-        return True 
-    else:
-        return False
-                 
-#loop del juego
-se_ejecuta= True
-
+# loop del juego
+se_ejecuta = True
 while se_ejecuta:
-    #imagen de fondo
-    pantalla.blit(fondo,(0,0))
-    #evento cerrar
+
+    pantalla.blit(fondo, (0, 0))
+
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             se_ejecuta = False
-        #evento presionar teclas
-        if evento.type  ==pygame.KEYDOWN:
-            if evento.key ==pygame.K_LEFT:
-                jugador_x_cambio -=0.3
-            if evento.key ==pygame.K_RIGHT:
-                jugador_x_cambio +=0.3  
+
+        if evento.type == pygame.KEYDOWN:
+            if evento.key == pygame.K_LEFT:
+                jugador_x_cambio = -0.3
+            if evento.key == pygame.K_RIGHT:
+                jugador_x_cambio = 0.3
             if evento.key == pygame.K_SPACE:
                 if not bala_visible:
+                    bala_x = jugador_x
+                    disparar_bala(bala_x, bala_y)
 
-                    bala_x=jugador_x
-                    disparar_bala(bala_x,bala_y)
+        if evento.type == pygame.KEYUP:
+            if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
+                jugador_x_cambio = 0
 
-        #evento soltar teclas 
-        if evento.type  ==pygame.KEYUP:
-            if evento.key ==pygame.K_LEFT or evento.key ==pygame.K_RIGHT:
-                jugador_x_cambio =0
-        
-    #modificar ubicacion    del jugador        
-    jugador_x+=jugador_x_cambio
+    # movimiento jugador
+    jugador_x += jugador_x_cambio
 
-    #mantener dentro de bordes del jugador   
-    if jugador_x <=0:
-        jugador_x=0
-    elif jugador_x >=736:
-        jugador_x= 736
-    #modificar ubicacion    del enemigo      
-    enemigo_x+=enemigo_x_cambio
+    if jugador_x <= 0:
+        jugador_x = 0
+    elif jugador_x >= 736:
+        jugador_x = 736
 
-    #mantener dentro de bordes del enemigo 
-    if enemigo_x <=0:
-        enemigo_x_cambio=0.3
-        enemigo_y+=enemigo_y_cambio
-    elif enemigo_x >=736:
-        enemigo_x_cambio=-0.3
-        enemigo_y+=enemigo_y_cambio
+    # movimiento enemigos
+    for e in range(cantidad_enemigos):
 
-    #movimiento bala
-    if bala_y <=-64:
-        bala_y=500 
-        bala_visible= False
+        enemigo_x[e] += enemigo_x_cambio[e]
+
+        if enemigo_x[e] <= 0:
+            enemigo_x_cambio[e] = 0.3
+            enemigo_y[e] += enemigo_y_cambio[e]
+        elif enemigo_x[e] >= 736:
+            enemigo_x_cambio[e] = -0.3
+            enemigo_y[e] += enemigo_y_cambio[e]
+
+        # colisión
+        colision = hay_colision(enemigo_x[e], enemigo_y[e], bala_x, bala_y)
+        if colision:
+            bala_y = 500
+            bala_visible = False
+            puntaje += 1
+            print("Puntaje:", puntaje)
+            enemigo_x[e] = random.randint(0, 736)
+            enemigo_y[e] = random.randint(50, 200)
+
+        enemigo(enemigo_x[e], enemigo_y[e], e)
+
+    # movimiento bala
+    if bala_y <= -64:
+        bala_y = 500
+        bala_visible = False
+
     if bala_visible:
-        disparar_bala(bala_x,bala_y)
-        bala_y-= bala_y_cambio
+        disparar_bala(bala_x, bala_y)
+        bala_y -= bala_y_cambio
 
-    #colision
-    colision=hay_colision(enemigo_x,enemigo_y,bala_x,bala_y)
-    if colision:
-        bala_y=500 
-        bala_visible= False 
-        puntaje +=1
-        print(puntaje)
-        enemigo_x= random.randint(0,736)
-        enemigo_y = random.randint(50,200)
-        
-
-    jugador(jugador_x,jugador_y)
-    enemigo(enemigo_x,enemigo_y)
-    #actualizar
+    jugador(jugador_x, jugador_y)
     pygame.display.update()
-
-    
