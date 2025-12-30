58b22f991b2bf365bbd0ee67008bf9bde2d311b1
Camilo Diaz <camilo20263@gmail.com>
2025-12-30 13:36:13 -0500

    agregar movimiento al enemigo

--- dia10/main.py
+++ dia10/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random 
 #inicializar pygame
 pygame.init()
 
@@ -19,11 +20,24 @@ jugador_x= 368
 jugador_y = 528
 jugador_x_cambio=0
 
+#enemigo
+img_enemigo = pygame.image.load("enemigo.png")
+enemigo_x= random.randint(0,736)
+enemigo_y = random.randint(50,200)
+enemigo_x_cambio=0.3
+enemigo_y_cambio=50
+
+
+
 
 #funcion jugador
 def jugador(x,y):
     pantalla.blit(img_jugador,(x,y))
 
+
+#funcion enemigo
+def enemigo(x,y):
+    pantalla.blit(img_enemigo,(x,y))
 
 #loop del juego
 se_ejecuta= True
@@ -37,23 +51,36 @@ while se_ejecuta:
         #evento presionar flechas 
         if evento.type  ==pygame.KEYDOWN:
             if evento.key ==pygame.K_LEFT:
-                jugador_x_cambio -=0.1
+                jugador_x_cambio -=0.3
             if evento.key ==pygame.K_RIGHT:
-                jugador_x_cambio +=0.1  
+                jugador_x_cambio +=0.3  
         #evento soltar flechas 
         if evento.type  ==pygame.KEYUP:
             if evento.key ==pygame.K_LEFT or evento.key ==pygame.K_RIGHT:
                 jugador_x_cambio =0
-    #modificar ubicacion            
+    #modificar ubicacion    del jugador        
     jugador_x+=jugador_x_cambio
 
-    #mantener dentro de bordes
+    #mantener dentro de bordes del jugador   
     if jugador_x <=0:
         jugador_x=0
-    if jugador_x >=736:
+    elif jugador_x >=736:
         jugador_x= 736
+    #modificar ubicacion    del enemigo      
+    enemigo_x+=enemigo_x_cambio
+
+    #mantener dentro de bordes del enemigo 
+    if enemigo_x <=0:
+        enemigo_x_cambio=0.3
+        enemigo_y+=enemigo_y_cambio
+    elif enemigo_x >=736:
+        enemigo_x_cambio=-0.3
+        enemigo_y+=enemigo_y_cambio
+
+        
 
     jugador(jugador_x,jugador_y)
+    enemigo(enemigo_x,enemigo_y)
     #actualizar
     pygame.display.update()
 
