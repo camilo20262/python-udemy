23f439ad2846257da71069099d207413f5a83be4
Camilo Diaz <camilo20263@gmail.com>
2025-12-30 13:16:58 -0500

    mantener dentro de bordes

--- dia10/main.py
+++ dia10/main.py
@@ -19,6 +19,8 @@ jugador_x= 368
 jugador_y = 528
 jugador_x_cambio=0
 
+
+#funcion jugador
 def jugador(x,y):
     pantalla.blit(img_jugador,(x,y))
 
@@ -28,21 +30,31 @@ se_ejecuta= True
 
 while se_ejecuta:
     pantalla.fill((219, 251, 251))
-    
+    #evento cerrar
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             se_ejecuta = False
-
+        #evento presionar flechas 
         if evento.type  ==pygame.KEYDOWN:
             if evento.key ==pygame.K_LEFT:
                 jugador_x_cambio -=0.1
             if evento.key ==pygame.K_RIGHT:
-                jugador_x_cambio +=0.1   
+                jugador_x_cambio +=0.1  
+        #evento soltar flechas 
         if evento.type  ==pygame.KEYUP:
             if evento.key ==pygame.K_LEFT or evento.key ==pygame.K_RIGHT:
                 jugador_x_cambio =0
+    #modificar ubicacion            
     jugador_x+=jugador_x_cambio
 
+    #mantener dentro de bordes
+    if jugador_x <=0:
+        jugador_x=0
+    if jugador_x >=736:
+        jugador_x= 736
+
     jugador(jugador_x,jugador_y)
+    #actualizar
     pygame.display.update()
+
     
