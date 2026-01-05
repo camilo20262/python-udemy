192dbcf5da8e7ac02dd6b5196807e223a80aac75
Camilo Diaz <camilo20263@gmail.com>
2026-01-05 11:34:19 -0500

    obtener el contendio de la clase necesaria

--- dia11/proyecto.py
+++ dia11/proyecto.py
@@ -3,6 +3,9 @@ import requests
 
 url_base=  'https://books.toscrape.com/catalogue/category/books_1/page-{}.html'
 
-for n in range(1,11):
-    print(url_base.format(n))
+resultado= requests.get(url_base.format('1'))
+sopa = bs4.BeautifulSoup(resultado.text,'lxml')
+
+libros=sopa.select('.product_pod')
+print(libros)
 
