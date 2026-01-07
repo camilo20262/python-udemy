dc8ebee6ae98edccfc2649b006eac7e3bef267bb
Camilo Diaz <camilo20263@gmail.com>
2026-01-07 14:03:44 -0500

    codigo para convertir audio en texto

--- dia13/asistente_virtual.py
+++ dia13/asistente_virtual.py
@@ -7,16 +7,47 @@ import webbrowser
 import datetime
 import wikipedia
 
-#escuchar nuestro microfono y devolver el audio como texto
-
-
+# Escuchar nuestro micrófono y devolver el audio como texto
 def transformar_audio_text():
-    
+
     # almacenar recognizer en variable
-    r= sr.Regonizer()
-    #configurar el micforno 
-    with sr.Microfone() as origin:
-        #tiempo de espera 
-        r.pause_threshold= 0.8
-        #informar que comenzo la grabacion
-        print('ya puedes hablar')
+    r = sr.Recognizer()
+
+    # configurar el micrófono
+    with sr.Microphone() as origen:
+        # tiempo de espera
+        r.pause_threshold = 0.8
+
+        # informar que comenzó la grabación
+        print('Ya puedes hablar...')
+        
+        # guardar lo que escuche
+        audio = r.listen(origen)
+
+        try:
+            # reconocer audio con Google
+            pedido = r.recognize_google(audio, language='es-CO')
+
+            # prueba de que pudo ingresar
+            print('Dijiste:', pedido)
+
+            # devolver pedido
+            return pedido
+
+        # en caso de que no comprenda el audio
+        except sr.UnknownValueError:
+            print('Ups, no entendí')
+            return 'sigo esperando'
+
+        except sr.RequestError:
+            print('Ups, error con el servicio')
+            return 'sigo esperando'
+
+        # error inesperado
+        except Exception as e:
+            print('Ups, algo ha salido mal:', e)
+            return 'sigo esperando'
+
+
+# Ejecutar función
+transformar_audio_text()
