4635274f1b66f7e28dbc72315406784f44034455
Camilo Diaz <camilo20263@gmail.com>
2026-01-07 14:20:22 -0500

    seleccionar diferentes voces

--- dia13/asistente_virtual.py
+++ dia13/asistente_virtual.py
@@ -59,8 +59,15 @@ transformar_audio_text()
 def hablar(mensaje):
     #encender el motor de pyttsx3
     engine= pyttsx3.init()
-
+    engine.setProperty('voice',id3)
     #pronunciar mensaje 
     engine.say(mensaje)
     engine.runAndWait()
 
+
+
+id1= 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0'
+id2='HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0'
+id3='HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_ES-MX_SABINA_11.0'
+
+hablar('Hola nicol, espero que tengas un buen dia')
