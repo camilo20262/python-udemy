8797beab337d84904d958233c9cbe560f0aaeb57
Camilo Diaz <camilo20263@gmail.com>
2026-01-08 16:22:13 -0500

    pasar imagenes a rgb

--- dia14/reconocimiento_facial.py
+++ dia14/reconocimiento_facial.py
@@ -5,3 +5,7 @@ import face_recognition as fr
 # Cargar la imagen con la que se va a comparar
 foto_control = fr.load_image_file('FotoA.jpg')
 foto_prueba = fr.load_image_file('FotoB.jpg')
+
+# pasar imagenes a rgb
+foto_control = cv2.cvtColor(foto_control, cv2.COLOR_BGR2RGB)
+foto_prueba = cv2.cvtColor(foto_prueba, cv2.COLOR_BGR2RGB)
